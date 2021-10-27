fcb1813475f7543e9a0d837d8a50d91a066c4a09
vittosba <vitsbardella@gmail.com>
2021-10-27 19:10:15 +0200

    setup

---
+++ js/main.js
@@ -0,0 +1,42 @@
+/**
+ * JS CAROUSEL
+ *
+ * Dati tre array contenenti
+ * una lista ordinata di 5 immagini e
+ * una lista ordinata dei relativi 5 luoghi,
+ * e una lista di 5 testi.
+ * creare un carosello come nella foto alegata
+ *
+ * Al click dell'utente sulle frecce l'immagine attiva diventa
+ * visibile in formato grande a sinistra e
+ * nel suo angolo in basso a destra dovranno essere aggiunti i relativi titolo e il testo.
+ * Aggiorniamo anche la thumbnail attiva.
+ */
+
+// Immagini
+const imageCollection = [
+    './img/01.jpg',
+    './img/02.jpg',
+    './img/03.jpg',
+    './img/04.jpg',
+    './img/05.jpg',
+];
+
+// Titoli
+const titleCollection = [
+    'Svezia',
+    'Svizzera',
+    'Gran Bretagna',
+    'Germania',
+    'Paradise',
+];
+
+// Testi
+const textCollection = [
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+    'Lorem ipsum',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+    'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+];
+
